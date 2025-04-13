4f4dbd4a2c49d9fe3bbf3de4a0141c520211d58c
fcgith <steliyan.slavov31@icloud.com>
2025-04-13 18:12:00 +0300

    main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-#comm
+from fastapi import FastAPI
+
+app = FastAPI()
+
+# routers go here
+#
+# app.include_router(....)
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
