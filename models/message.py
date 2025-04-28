928f7ca2b0085e212e181fcd514fda1c998a8131
Martin <martin.kitukov@gmail.com>
2025-04-29 00:05:35 +0300

    updated message module

--- models/message.py
+++ models/message.py
@@ -14,3 +14,11 @@ class MessageCreate(BaseModel):
     content: str
     conversation_id: int
     sender_id: int
+
+class MessageRequest(BaseModel):
+    content: str
+
+
+class NewConversationRequest(BaseModel):
+    receiver_id: int
+    content: str
