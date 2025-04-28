1d336095bb97182abb3b3b1e13786cb6e65d069e
Martin <martin.kitukov@gmail.com>
2025-04-28 19:46:58 +0300

    message model created

---
+++ models/message.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+from pydantic import BaseModel
+
+
+class Message(BaseModel):
+    id: int | None = None
+    content: str
+    date: datetime
+    conversation_id: int
+    sender_id: int
+
+
+class MessageCreate(BaseModel):
+    content: str
+    conversation_id: int
+    sender_id: int
