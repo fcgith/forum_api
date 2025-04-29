6e0346961275688034860f335adea719de09c980
fcgith <steliyan.slavov31@icloud.com>
2025-04-29 20:09:35 +0300

    Handle Pydantic exception validation

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from fastapi import FastAPI
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
+from starlette.status import HTTP_400_BAD_REQUEST
+
 from routers.auth import router as auth_router
 from routers.user import router as user_router
 from routers.conversations import router as conversation_router
@@ -11,6 +15,17 @@ app.include_router(user_router, prefix="/users")
 app.include_router(conversation_router, prefix="/conversations")
 
 
+# Handle Pydantic exception validation error for frontend
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request, exc: RequestValidationError):
+    first_error = exc.errors()[0]["msg"].replace("Value error,", "Invalid data:")
+    return JSONResponse\
+    (
+        status_code=HTTP_400_BAD_REQUEST,
+        content={"detail": first_error}
+    )
+
+
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8001, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
